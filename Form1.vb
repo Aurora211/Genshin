33ba0bce92b03d12d7f68132e8a9a0aa85873816
Aurora211 <1422501924@qq.com>
2020-10-29 08:21:02 +0800

    bug fixed

--- Form1.vb
+++ Form1.vb
@@ -83,14 +83,20 @@ Public Class Form1
             Exit Sub
         End If
         Dim info As String() = ReadAllLines(Install_Path.Text & "\Genshin Impact Game\config.ini")
-        If info(2).Replace("cps=", "") = "bilibili" Then
-            Server.Text = "Bilibili Server"
-        ElseIf info(2).Replace("cps=", "") = "pcad360pz" Then
-            Server.Text = "Offical Server"
-        Else
-            Server.Text = "Unsupported Server"
-        End If
-        Channel.Text = info(1).Replace("channel=", "")
+        For index = 0 To (info.Length - 1)
+            If info(index).Contains("cps=") Then
+                If info(index).Contains("bilibili") Then
+                    Server.Text = "Bilibili Server"
+                ElseIf info(2).Replace("cps=", "") = "pcad360pz" Or info(index).Contains("mihayo") Then
+                    Server.Text = "Offical Server"
+                Else
+                    Server.Text = "Unsupported Server"
+                End If
+            End If
+            If info(index).Contains("channel=") And info(index).Contains("sub_channel=") = False Then
+                Channel.Text = info(index).Replace("channel=", "")
+            End If
+        Next
         Server.ForeColor = Color.Black
         Channel.ForeColor = Color.Black
         GC.Collect()
@@ -175,11 +181,14 @@ Public Class Form1
     Private Sub Bilibili_Click(sender As Object, e As EventArgs) Handles Bilibili.Click
         Dim info As String() = ReadAllLines(Install_Path.Text & "\Genshin Impact Game\config.ini")
         For index = 0 To (info.Length - 1)
-            If info(index).Contains("channel=") Then
+            If info(index).Contains("channel=") And info(index).Contains("sub_channel=") = False Then
                 info(index) = "channel=14"
             End If
             If info(index).Contains("cps=") Then
-                info(2) = "cps=bilibili"
+                info(index) = "cps=bilibili"
+            End If
+            If info(index).Contains("sub_channel=") Then
+                info(index) = "sub_channel=0"
             End If
         Next
         Dim fin As String = info(0)
@@ -193,13 +202,15 @@ Public Class Form1
 
     Private Sub Official_Click(sender As Object, e As EventArgs) Handles Official.Click
         Dim info As String() = ReadAllLines(Install_Path.Text & "\Genshin Impact Game\config.ini")
-        MsgBox(info.Length)
         For index = 0 To (info.Length - 1)
-            If info(index).Contains("channel=") Then
+            If info(index).Contains("channel=") And info(index).Contains("sub_channel=") = False Then
                 info(index) = "channel=1"
             End If
             If info(index).Contains("cps=") Then
-                info(2) = "cps=mihayou"
+                info(index) = "cps=mihayo"
+            End If
+            If info(index).Contains("sub_channel=") Then
+                info(index) = "sub_channel=1"
             End If
         Next
         Dim fin As String = info(0)
